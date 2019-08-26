fc07d929112e8db4e3758d9e9f1ba27bcb78cfff
Dewa <dewabrata@gmail.com>
2019-08-26 12:44:12 +0700

    First Initiation

---
+++ webservice-attendence/src/main/java/com/webservice/attendence/WebserviceAttendenceApplication.java
@@ -0,0 +1,13 @@
+package com.webservice.attendence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceAttendenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceAttendenceApplication.class, args);
+	}
+
+}
---
+++ webservice-attendence/src/test/java/com/webservice/attendence/WebserviceAttendenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservice.attendence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceAttendenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
