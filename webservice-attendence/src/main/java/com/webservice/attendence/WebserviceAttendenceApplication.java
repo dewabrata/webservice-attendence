d35fcae29bdf355684c7588723c67ef6d4ab39c7
Dewa <dewabrata@gmail.com>
2019-08-26 14:30:41 +0700

    test

--- webservice-attendence/src/main/java/com/webservice/attendence/WebserviceAttendenceApplication.java
+++ webservice-attendence/src/main/java/com/webservice/attendence/WebserviceAttendenceApplication.java
@@ -8,6 +8,7 @@ public class WebserviceAttendenceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebserviceAttendenceApplication.class, args);
+		System.out.println("");
 	}
 
 }
